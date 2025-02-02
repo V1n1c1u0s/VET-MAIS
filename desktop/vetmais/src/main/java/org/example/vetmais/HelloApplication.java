4f0c2630ca2053c24d93074cc64e57fc2cd89c50
V1n1c1u0s <caiovinicius2700@gmail.com>
2025-02-02 05:34:41 -0300

    first-splash

--- desktop/vetmais/src/main/java/org/example/vetmais/HelloApplication.java
+++ desktop/vetmais/src/main/java/org/example/vetmais/HelloApplication.java
@@ -3,16 +3,22 @@ package org.example.vetmais;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/login/fxml/new.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/login/fxml/new.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/Splash/fxml/splash.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
+        //stage.setOpacity(0.5);
         stage.show();
     }
 
